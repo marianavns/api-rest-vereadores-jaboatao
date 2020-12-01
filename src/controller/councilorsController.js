d6e1673d101d869d1f4041b084b0112632b86399
marianavns <mariana.vns@gmail.com>
2020-12-01 12:09:41 -0300

    feat(councilors): adds read by parameter (readByName) and query method (search).

--- src/controller/councilorsController.js
+++ src/controller/councilorsController.js
@@ -14,16 +14,41 @@ const create = (req, res) => {
 }
 
 const readAll = (req, res) => {
-    councilors.find(function (err, councilor) {
+    councilors.find(function (err, results) {
         if (err) {
             res.status(500).send({ message: err.message })
         } else {
-            res.status(200).send(councilor)
+            res.status(200).send(results)
         }
     })
 }
 
+const readByName = (req, res) => {
+    const name = req.params.campaignName
+    councilors.find({ campaignName : name }, function (err, result) {
+        if (err) {
+            res.status(500).send({ message: err.message })
+        } else {
+            res.status(200).send(result)
+        }
+    })
+}
+
+const readBySearch = (req, res) => {
+    const search = req.query
+    councilors.find(search, function (err, result) {
+        if (err) {
+            res.status(500).send({ message: err.message })
+        } else {
+            res.status(200).send(result)
+        }
+    })
+}
+   
+
 module.exports = {
     create,
-    readAll
+    readAll,
+    readByName,
+    readBySearch
 }
--- src/routes/councilorsRoute.js
+++ src/routes/councilorsRoute.js
@@ -5,8 +5,10 @@ const router = express.Router()
 const controller = require('../controller/councilorsController')
 
 router.post('/', controller.create)
+router.get('/', controller.readBySearch)
 router.get('/', controller.readAll)
-// router.get('/', controller.getByConclusion)
+router.get('/:campaignName', controller.readByName)
+
 
 // router.delete('', controller.delete)
 // router.delete('/', controller.delete)
