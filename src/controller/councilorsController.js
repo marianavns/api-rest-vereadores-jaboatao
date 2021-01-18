9a83b773fdf55d225fe47c6f84934572d8b6aa80
marianavns <mariana.vns@gmail.com>
2021-01-18 19:03:24 -0300

    feat(read): creates readByCNPJ and another refactorings.

--- src/controller/councilorsController.js
+++ src/controller/councilorsController.js
@@ -3,14 +3,16 @@ const councilors = require( '../model/councilors.js' )
 const fs = require('fs')
 
 const create = (req, res) => {
-
     let councilor = new councilors(req.body)
     councilor.save(function(err){
+        // if (err.name === 'MongoError' && err.code === 11000) {
+            // return res.status(500).send({ message: "Este CNPJ de campanha já foi cadastrado." })
+        // } 
         if (err) {
-            res.status(500).send({ message: err.message })
-        } else {
-            res.status(201).send({ message : `councilor [${criticalData1}] was added successfully.`})
-        }
+            return res.status(500).send({ message: err.message })
+        } 
+            return res.status(201).send({ message : `O(A) vereador(a) ${req.body.campaignName} foi adicionado com sucesso.`})
+        
     })
 }
 
@@ -18,68 +20,103 @@ const create = (req, res) => {
 const readAll = (req, res) => {
     councilors.find(function (err, results) {
         if (err) {
-            res.status(500).send({ message: err.message })
-        } else {
-            res.status(200).send(results)
-        }
+            return res.status(500).send({ message: err.message })
+        } 
+        return res.status(200).send(results)
     })
 }
 
-const readBills = (req, res) => {
+
+const readAllBills = (req, res) => {
     councilors.find(
-        { 
-            inActivity : true 
-        }, 
-        '-_id campaignName fullName mandateRemuneration lastCampaignExpenses campaignExpensesLink', 
+        { inActivity : true }, 
+        '-_id campaignName campaignCnpj lastCampaignParty monthlyBasisRemuneration totalRemuneration lastCampaignExpenses campaignExpensesLink email', 
         function (err, results) {
             if (err) {
-                res.status(500).send({ message: err.message })
-            } else {
-                res.status(200).send(results)
-            }
+               return res.status(500).send({ message: err.message })
+            } 
+            return res.status(200).send(results)
         }
     )
 }
 
+
 const readByName = (req, res) => {
-    const inputName = req.query.firstName
-    councilors.find( { firstName: inputName } , function (err, result) {
-        if (err) {
-            res.status(500).send({ message: err.message })
-        } else {
-            res.status(200).send(result)
+    const inputName = req.query.nome
+    councilors.find( 
+        { $or: [ {firstName : inputName}, {campaignName : inputName}] }, 
+        function (err, result) {
+            if (err) {
+                return res.status(500).send({ message: err.message })
+            }
+        return res.status(200).send(result)
         }
-    })
+    )
 }
 
-const updateItemsByName = (req, res) => {
-    const firstName = req.params.firstName
-    const updateEntry = req.body
-    councilors.updateMany({ firstName }, { $set : updateEntry }, { upsert : true }, function(err){
-        if (err) {
-            res.status(500).send({ message: err.message })
-        } else {
-            res.status(200).send({ message : `Attributes: [${Object.keys(updateEntry)}] have been updated successfully.`})
+
+const readBillsByName = (req, res) => {
+    const inputName = req.query.nome
+    councilors.find(
+        { $or: [ {firstName : inputName}, {campaignName : inputName}] }, 
+        '-_id campaignName campaignCnpj lastCampaignParty monthlyBasisRemuneration totalRemuneration lastCampaignExpenses campaignExpensesLink email', 
+        function (err, results) {
+            if (err) {
+               return res.status(500).send({ message: err.message })
+            } 
+            return res.status(200).send(results)
         }
-    })
+    )
+}
+
+
+const readByCNPJ = (req, res) => {
+    const inputCNPJ = req.query.cnpjdecampanha
+    councilors.find(
+        { campaignCnpj: inputCNPJ }, 
+        function (err, result) {
+            if (err) {
+                return res.status(500).send({ message: err.message })
+            } 
+            return res.status(200).send(result)
+        }
+    )
+}
+
+
+const updateByName = (req, res) => {
+    const firstName = req.query.nome
+    const updateEntry = req.body
+    councilors.updateMany(
+        { firstName }, 
+        { $set : updateEntry }, 
+        { upsert : false  }, 
+        function(err){
+            if (err) {
+                return res.status(500).send({ message: err.message })
+            }
+            return res.status(200).send({ message : `Os atributos [ ${Object.keys(updateEntry)} ] foram atualizados com sucesso.`})
+        }
+    )
 }
 
 const deleteByDocID = (req, res) => {
     const id = req.params._id
     councilors.deleteMany({ _id : id }, function(err){
         if (err) {
-            res.status(500).send({ message: err.message })
-        } else {
-            res.status(200).send({ message : `ID Doc ${id} was been deleted.`})
-        }
+            return res.status(500).send({ message: err.message })
+        } 
+           return res.status(200).send({ message : `O(A) vereador(a) com o ${id} foi deletado.`})
     })
 }
 
 module.exports = {
     create,
     readAll,
-    readBills,
+    readAllBills,
     readByName,
-    updateItemsByName,
+    readByCNPJ,
+    readBillsByName,
+    updateByName,
     deleteByDocID
 }
