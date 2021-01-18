ea15895a2dd6b487e3880a47fc089e9b7cc09af5
marianavns <mariana.vns@gmail.com>
2021-01-18 19:10:19 -0300

    feat(routes/councilors): adds readbyCNPJ route and other refactorings.

--- src/routes/councilorsRoute.js
+++ src/routes/councilorsRoute.js
@@ -6,9 +6,11 @@ const controller = require('../controller/councilorsController')
 
 router.post('/', controller.create)
 router.get('/', controller.readAll)
-router.get('/contas', controller.readBills)
-router.get('/buscaNome', controller.readByName)
-router.patch('/:firstName', controller.updateItemsByName)
+router.get('/contas', controller.readAllBills)
+router.get('/nome', controller.readByName)
+router.get('/contasindividuais', controller.readBillsByName)
+router.get('/cnpj', controller.readByCNPJ)
+router.patch('/', controller.updateByName)
 router.delete('/:_id', controller.deleteByDocID)
 
 module.exports = router
